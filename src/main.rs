a5aeff39b50d87ec9f7c97a5defb1c0cb4cc6499
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2023-01-17 08:53:43 -0300

    if let control flow

--- src/main.rs
+++ src/main.rs
@@ -155,4 +155,45 @@ fn main() {
         fn remove_fancy_hat() {}
         fn reroll() {}
     }
+    {
+        let config_max = Some(3u8);
+        match config_max {
+            Some(max) => println!("The maximum is configured to be {}", max),
+            _ => (),
+        }
+        //Same with if let
+        let config_max = Some(3u8);
+        if let Some(max) = config_max {
+            println!("The maximum is configured to be {}", max);
+        }
+    }
+    {
+        #[derive(Debug)] // so we can inspect the state in a minute
+        enum UsState {
+            Alabama,
+            Alaska,
+        }
+
+        enum Coin {
+            Penny,
+            Nickel,
+            Dime,
+            Quarter(UsState),
+        }
+        let coin = Coin::Penny;
+        let mut count = 0;
+        match coin {
+            Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+            _ => count += 1,
+        }
+
+        // Same way
+        let coin = Coin::Penny;
+        let mut count = 0;
+        if let Coin::Quarter(state) = coin {
+            println!("State quarter from {:?}!", state);
+        } else {
+            count += 1;
+        }
+    }
 }
